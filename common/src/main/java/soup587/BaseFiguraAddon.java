3f11584b4ca916db59532de43cd52c82d10e2789
jess <jr587atroblox@gmail.com>
2025-03-02 12:28:02 +0000

    it's alive

---
+++ common/src/main/java/soup587/BaseFiguraAddon.java
@@ -0,0 +1,9 @@
+package soup587;
+
+public final class BaseFiguraAddon {
+    public static final String MOD_ID = "basefiguraaddon";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric/src/main/java/soup587/fabric/BaseFiguraAddonFabric.java
@@ -0,0 +1,17 @@
+package soup587.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+import soup587.BaseFiguraAddon;
+
+public final class BaseFiguraAddonFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        // Run our common setup.
+        BaseFiguraAddon.init();
+    }
+}
